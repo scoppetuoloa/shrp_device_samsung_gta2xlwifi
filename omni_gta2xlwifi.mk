c21fde596f19882141b354f4e41575d0c115a44c
Lee Scotter <Scotter1977@mailfence.com>
2021-10-12 23:05:37 +0100

    Some updates

--- omni_gta2xlwifi.mk
+++ omni_gta2xlwifi.mk
@@ -1,32 +1,12 @@
-#
-# Copyright (C) 2020 The Android Open Source Project
-# Copyright (C) 2020 The TWRP Open Source Project
-# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+# Release name
+PRODUCT_RELEASE_NAME := gta2xlwifi
 
-# Inherit from those products. Most specific first.
-$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from gta2xlwifi device
-$(call inherit-product, device/samsung/gta2xlwifi/device.mk)
-
-# Inherit some common Omni stuff.
+# Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := gta2xlwifi
@@ -34,4 +14,3 @@ PRODUCT_NAME := omni_gta2xlwifi
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-T590
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_RELEASE_NAME := samsung samsung
